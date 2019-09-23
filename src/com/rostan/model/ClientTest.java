dd4be17774161a77f8c88e69b28ebb8234305f9e
Rostanxd <allowed_g@hotmail.com>
2019-09-22 19:16:52 -0500

    Initial commit

---
+++ src/com/rostan/model/ClientTest.java
@@ -0,0 +1,29 @@
+package com.rostan.model;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class ClientTest {
+    public static void main(String[] args) {
+        final String HOST = "127.0.0.1";
+        final int PUERTO = 1000;
+        DataInputStream in;
+        DataOutputStream out;
+
+        try {
+            Socket socket = new Socket(HOST, PUERTO);
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+            out.writeUTF("Hola desde el cliente");
+            System.out.println(in.readUTF());
+            socket.close();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/com/rostan/model/ServerTest.java
@@ -0,0 +1,35 @@
+package com.rostan.model;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerTest {
+    public static void main(String[] args) {
+        ServerSocket serverSocket = null;
+        Socket sc = null;
+        DataInputStream in;
+        DataOutputStream out;
+
+        final int PUERTO = 1000;
+
+        try {
+            serverSocket = new ServerSocket(PUERTO);
+            System.out.println("Servidor iniciado");
+            while (true) {
+                sc = serverSocket.accept();
+                System.out.println("Cliente conectado!");
+                in = new DataInputStream(sc.getInputStream());
+                out = new DataOutputStream(sc.getOutputStream());
+                System.out.println(in.readUTF());
+                out.writeUTF("Hola mundo desde el servidor");
+                sc.close();
+                System.out.println("Cliente desconectado!");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
